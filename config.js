8f1e9fd62cbd0a2ff713b5b5491b0d25667bef07
ben509-bo <c12386791@gmail.com>
2025-01-22 23:11:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '50941610347' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || '6RIQ1ZgA#5CR7Yj0l6SbVNAXh3-VsxY32VXxB3Q_HwWLpujzqbWQ' ;  // put your session id here
 
 global.mods = []
 global.prems = []
